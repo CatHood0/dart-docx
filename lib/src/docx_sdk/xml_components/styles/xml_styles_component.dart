9f2eb4db2b8978673686d6cff6543b0eef9be0be
cathood0 <santiagowmar@gmail.com>
2026-06-01 22:38:21 -0400

    Fix: again, another missundertood with how works latent ones

--- lib/src/docx_sdk/xml_components/styles/xml_styles_component.dart
+++ lib/src/docx_sdk/xml_components/styles/xml_styles_component.dart
@@ -6,6 +6,7 @@ import 'xml_default_doc_styles_component.dart';
 class XmlStylesComponent extends XmlComponentBase<void> {
   XmlStylesComponent({
     required this.docStyles,
+    this.tree,
   }) : super(
           xmlKey: 'w:styles',
           value: null,
@@ -18,6 +19,7 @@ class XmlStylesComponent extends XmlComponentBase<void> {
         );
 
   final DocumentStyles docStyles;
+  final DocxNode? tree;
 
   @override
   String get name => 'Styles';
@@ -28,13 +30,45 @@ class XmlStylesComponent extends XmlComponentBase<void> {
   @override
   XmlElement buildXml() {
     CompilerLogger.root.info('Analyzing styles to build LatentStyles');
+    final List<Style> styles = [];
+    tree?.visitAllElement(
+      (e) {
+        if (e is Paragraph) {
+          styles.addAll(e.styles.where((Style e) => !e.isReference));
+        }
+
+        if (e is Table) {
+          styles.addAll(
+              e.tableProperties!.styles.where((Style e) => !e.isReference));
+        }
+        if (e.child is ImageData) {
+          styles.addAll(
+              (e.child as ImageData).styles.where((e) => !e.isReference));
+        }
+
+        if (e is TextRun) {
+          styles.addAll(e.child.styles.where((Style e) => !e.isReference));
+        }
+
+        if (e is HyperlinkRun) {
+          styles.addAll(e.child.styles.where((Style e) => !e.isReference));
+        }
+
+        if (e is Text) {
+          styles.addAll(e.styles.where((Style e) => !e.isReference));
+        }
+
+        return false;
+      },
+    );
+
     final (LatentStyles latent) = LatentAnalyzer.analyze(
-      docStyles.styles.values.toList(),
+      styles,
       docStyles.latentStyles,
     );
     CompilerLogger.root.info(
       'Building styles.xml component. '
-      'Built-in styles count: ${docStyles.styles.length}. '
+      'Built-in styles count: ${styles.length}. '
       'latentStyles processed count: ${latent.count}, '
       'exceptions: ${latent.exceptions.length}',
     );
